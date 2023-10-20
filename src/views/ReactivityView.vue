70cae6bc4efe7b39dc2d4dcd01c60219bf298632
rock's ezweb <alikerock@gmail.com>
2023-10-20 18:11:58 +0900

    nextTick

--- src/views/ReactivityView.vue
+++ src/views/ReactivityView.vue
@@ -1,24 +1,49 @@
 <script setup>
-// import { ref } from 'vue'
-
-// const count = ref(0)
-import { reactive } from 'vue'
-
-const state = reactive({ count: 0 })
-console.log(state);
-
+/*
+import { ref } from 'vue'
+const count = ref(0)
+console.log(count);
 const increment = () => {
-  state.count++
+  count.value++
+  console.log(count);
+}
+*/
+
+
+/*
+  import { reactive } from 'vue'
+
+  const state = reactive({ count: 0 })
+  console.log(state);
+
+  const increment = () => {
+    state.count++
+  }
+*/
+import { ref, nextTick } from 'vue'
+
+const count = ref(0)
+
+async function increment() {
+  count.value++
+
+  // 아직 DOM 업데이트되지 않음.
+  console.log(document.getElementById('counter').textContent) // 0
+
+  await nextTick()
+  // 이제 DOM 업데이트됨.
+  console.log(document.getElementById('counter').textContent) // 1
 }
 
 </script>
 
 
 <template>
+  {{console.log('template 실행')}}
   <div class="reactivity">
     <h1>This is an Reactivity page</h1>
     <button @click="increment"> + </button>
-    <div>{{ state.count }}</div>
+    <div id="counter">{{ count }}</div>
   </div>
 </template>
 
